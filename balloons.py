cd893c841f6f943d86448e142f1d7bfbe5ffb067
Viacheslav Ivanov <slava.eth@gmail.com>
2014-01-10 18:18:47 -0800

    added pygame exapmle

--- balloons.py
+++ balloons.py
@@ -4,18 +4,40 @@ from Tkinter import *
 import random
 import time
 
+SCREEN_WIDTH  = 500
+SCREEN_HEIGHT = 500
+
+BALOON_RADIUS = 25
+BALOON_DIAMETER = 50
+
+class Bal
+
 class Baloon:
+  '''Our funky baloon'''
+  def __init__(self, canvas, type=0):
+    self.canvas = canvas
+    self.id = canvas.create_oval( 0, 0, BALOON_DIAMETER, BALOON_DIAMETER, fill='yellow' )
+    self.canvas.move( self.id, random.randrange( SCREEN_WIDTH ), SCREEN_HEIGHT )
 
-  def __init__(self, arg):
-    super(Baloon, self).__init__()
-    self.arg = arg
-
+  def draw(self):
+    self.canvas.move( self.id, 0, -1 )
 
 tk = Tk()
 tk.title( 'Pop The Baloons' )
 tk.resizable( 0, 0 )
 tk.wm_attributes( '-topmost', 1 )
-canvas = Canvas( tk, width=500, height=500, bd=0, highlightthickness=0 )
+canvas = Canvas( tk, width=SCREEN_WIDTH, height=SCREEN_HEIGHT, bd=0, highlightthickness=0 )
 canvas.pack()
 tk.update()
-tk.mainloop()
+
+balloons = []
+for x in range(1, 10):
+  balloons.append( Baloon( canvas ) )
+
+while 1:
+  for baloon in balloons:
+    baloon.draw()
+
+  tk.update_idletasks()
+  tk.update()
+  time.sleep( 0.01 )
---
+++ mygame.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import sys, pygame
+
+pygame.init()
+
+size = width, height = 600, 400
+
+screen = pygame.display.set_mode( size )
+
